bac31899c000a7b01608c385953defd5c7e760be
Panagiotis Zestanakis <panosz@gmail.com>
2018-10-26 21:05:40 +0300

    pendulum example. Demonstrate how some orbits that should close, throw a "never came back" exception

--- src/examples/pendulum_example.cpp
+++ src/examples/pendulum_example.cpp
@@ -41,16 +41,25 @@ ActionResult calculate_action(const State2& s_init)
 
   const IntegrationOptions options;
 
-  const auto integrationTime = Integrators::TimeInterval{0.0,100};
+  const auto integrationTime = Integrators::TimeInterval{0.0,1000};
 
 
   State2 s1= s_init;
-  
-  const auto actionAngleOrbit = calculate_action_angle_on_closed_orbit(hamiltonian,s1,integrationTime,options,2);
+  try
+    {
+      const auto actionAngleOrbit = calculate_action_angle_on_closed_orbit(hamiltonian, s1, integrationTime, options, 2);
 
-  result.numerical_action = actionAngleOrbit.action_two_pi()*boost::math::double_constants::one_div_two_pi;
-  result.omega = actionAngleOrbit.omega();
+      result.numerical_action = actionAngleOrbit.action_two_pi() * boost::math::double_constants::one_div_two_pi;
+      result.omega = actionAngleOrbit.omega();
+    }
+  catch (std::exception & e)
+    {
+      std::cout<<e.what()<<'\n';
+      std::cout<<"At orbit starting from point "<< s_init<<'\n';
+      result.numerical_action=std::nan("");
+      result.omega=std::nan("");
 
+    }
   return result;
 
 }
@@ -58,11 +67,11 @@ ActionResult calculate_action(const State2& s_init)
 int main ()
 {
 
-  const auto x_init_vals = PanosUtilities::linspace(0.5,0.9,30);
+  const auto x_init_vals = PanosUtilities::linspace(0.01,boost::math::double_constants::pi,30);
 
   std::vector<ActionResult> action_result_vector;
 
-  boost::push_back(action_result_vector, x_init_vals | boost::adaptors::transformed( [](auto x){return calculate_action(State2{x,0.8});}));
+  boost::push_back(action_result_vector, x_init_vals | boost::adaptors::transformed( [](auto x){return calculate_action(State2{x,0});}));
 
   std::cout<<"engergy\tanalytical_action\tnumerical_action\tomega\n";
   for (const auto& a_r: action_result_vector)
@@ -72,6 +81,17 @@ int main ()
         << a_r.omega<<'\n';
 
 
+  auto times = PanosUtilities::linspace(0.0, 10, 1000);
+
+  auto s_escaping_init = Geometry::State2{1.62979, 0};
+  auto orbit_range = make_interval_range(Dynamics::DynamicSystem(Hamiltonian::PendulumHamiltonian{1, 1}),
+                                         s_escaping_init, times, Integrators::IntegrationOptions{});
+
+  std::ofstream output("not_retunring.txt");
+
+  output<< "not returning orbit\n";
+  for (const auto & s: orbit_range)
+    output<<s.first<<'\n';
 //  for ( const auto s_t : boost::range::combine(actionAngleOrbit.positions(),actionAngleOrbit.theta()))
 //    {
 //      double theta;
